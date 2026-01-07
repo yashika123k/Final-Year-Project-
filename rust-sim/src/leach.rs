d658933c27e0714d1b94037371d1a2e0461a3682
Madhav Singh <msg200415@gmail.com>
2026-01-07 15:18:55 +0530

    build leach protocol

--- rust-sim/src/leach.rs
+++ rust-sim/src/leach.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 use rand::Rng;
 use crate::node::Node;
 use crate::config::{P,CYCLE};
@@ -21,20 +23,64 @@ fn is_eligible(node: &Node, round: i64) -> bool {
     false
 }
 
-pub fn select_cluster_heads(nodes: &mut[Node], round: i64) {
+pub fn build(nodes: &mut[Node], round: i64) {
     
     let threshold = threshold(round);
     let mut rng = rand::rng();
-    for node in nodes{
+
+    let mut cluster_heads: Vec<usize> = Vec::new();
+    for node in nodes.iter_mut(){
        if is_eligible(node, round){
            let rand_prob:f64 = rng.random();
 
            if rand_prob <= threshold{
                node.is_ch = true;
                node.last_ch_round = round;
+               cluster_heads.push(node.id);
            }
        }
     }
+
+    form_clusters(nodes, &cluster_heads);
 }
 
+
+
+fn form_clusters(nodes: &mut [Node], cluster_heads: &[usize]) {
+    // (node_id, ch_id)
+    let mut assignments: Vec<(usize, usize)> = Vec::new();
+
+   
+    for node in nodes.iter() {
+        if node.is_ch || !node.is_alive {
+            continue;
+        }
+
+        let mut min_dist = 0.0;
+        let mut chosen_ch: Option<usize> = None;
+
+        for &ch_id in cluster_heads {
+            let ch = &nodes[ch_id];
+
+            let dx = ch.position.x - node.position.x;
+            let dy = ch.position.y - node.position.y;
+            let dist = dx * dx + dy * dy;
+
+            if dist < min_dist {
+                min_dist = dist;
+                chosen_ch = Some(ch_id);
+            }
+        }
+
+        if let Some(ch_id) = chosen_ch {
+            assignments.push((node.id, ch_id));
+        }
+    }
+
+    
+    for (node_id, ch_id) in assignments {
+        nodes[node_id].cluster_id = Some(ch_id);
+        nodes[ch_id].members.push(node_id);
+    }
+}
 
